e1bd9bfe5526d7c389f7d7c620d50413be390487
Jameson Batista de Araujo <jameson@MMB013411CPS.local>
2021-04-20 11:20:17 -0300

    treinamento

--- src/main/java/runner/RegressionTest.java
+++ src/main/java/runner/RegressionTest.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"features"},
         snippets = CucumberOptions.SnippetType.CAMELCASE,
-        glue = {"steps"}
+        glue = {"steps"},
+        publish = true
 )
 public class RegressionTest {
 }
